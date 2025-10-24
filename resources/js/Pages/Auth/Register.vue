c7b68c497a92ab66f1ccb4833044cafae327f4c0
Haerul Yuda Aditiya <haerulyudaaditiya@gmail.com>
2025-10-24 09:54:34 +0700

    Fix: Kembali ke 'name' & perbaiki bug deployment cPanel

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -6,9 +6,9 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
-// PENYESUAIAN: Menggunakan 'username' bukan 'name'
+// 1. KEMBALI MENGGUNAKAN 'name'
 const form = useForm({
-    username: '',
+    name: '',
     email: '',
     password: '',
     password_confirmation: '',
@@ -23,26 +23,27 @@ const submit = () => {
 
 <template>
     <GuestLayout>
-        <Head title="Daftar" /> <!-- PENYESUAIAN: Judul -->
+        <Head title="Daftar" /> <!-- Sudah diterjemahkan -->
 
         <form @submit.prevent="submit">
-            <!-- PENYESUAIAN: Input 'Name' diganti dengan 'Username' -->
+            <!-- 2. KEMBALI MENGGUNAKAN INPUT 'name' -->
             <div>
-                <InputLabel for="username" value="Username" />
+                <InputLabel for="name" value="Nama" />
                 <TextInput
-                    id="username"
+                    id="name"
                     type="text"
                     class="mt-1 block w-full"
-                    v-model="form.username"
+                    v-model="form.name"
                     required
                     autofocus
-                    autocomplete="username"
+                    autocomplete="name"
                 />
-                <InputError class="mt-2" :message="form.errors.username" />
+                <InputError class="mt-2" :message="form.errors.name" />
             </div>
 
+            <!-- Input 'Email' -->
             <div class="mt-4">
-                <InputLabel for="email" value="Alamat Email" /> <!-- PENYESUAIAN: Teks -->
+                <InputLabel for="email" value="Alamat Email" />
                 <TextInput
                     id="email"
                     type="email"
@@ -54,8 +55,9 @@ const submit = () => {
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
+            <!-- Input 'Password' -->
             <div class="mt-4">
-                <InputLabel for="password" value="Kata Sandi" /> <!-- PENYESUAIAN: Teks -->
+                <InputLabel for="password" value="Kata Sandi" />
                 <TextInput
                     id="password"
                     type="password"
@@ -67,8 +69,9 @@ const submit = () => {
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
+            <!-- Input 'Konfirmasi Password' -->
             <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Konfirmasi Kata Sandi" /> <!-- PENYESUAIAN: Teks -->
+                <InputLabel for="password_confirmation" value="Konfirmasi Kata Sandi" />
                 <TextInput
                     id="password_confirmation"
                     type="password"
@@ -85,11 +88,11 @@ const submit = () => {
                     :href="route('login')"
                     class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                    Sudah punya akun? <!-- PENYESUAIAN: Teks -->
+                    Sudah punya akun?
                 </Link>
 
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Daftar <!-- PENYESUAIAN: Teks -->
+                    Daftar
                 </PrimaryButton>
             </div>
         </form>
