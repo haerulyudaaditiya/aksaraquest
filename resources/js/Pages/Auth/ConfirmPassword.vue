14afb344c0e844abd4ddaa903f0c779052077f36
Haerul Yuda Aditiya <haerulyudaaditiya@gmail.com>
2025-10-31 12:53:12 +0700

    Sesuaikan view confirm password agar mobile first

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -19,28 +19,40 @@ const submit = () => {
 
 <template>
     <GuestLayout>
-        <Head title="Konfirmasi Kata Sandi" /> <div class="mb-4 text-sm text-gray-600">
-            Ini adalah area aman dari aplikasi. Mohon konfirmasi kata sandi Anda sebelum melanjutkan.
+        <Head title="Konfirmasi Kata Sandi" />
+
+        <div class="max-w-md mx-auto bg-white p-6 rounded-lg shadow-sm">
+            <h2 class="text-center text-2xl font-semibold mb-4">
+                Konfirmasi Kata Sandi
+            </h2>
+
+            <p class="text-sm text-gray-600 mb-6 leading-relaxed text-center">
+                Ini adalah area aman dari aplikasi. Mohon konfirmasi kata sandi Anda sebelum melanjutkan.
+            </p>
+
+            <form @submit.prevent="submit" class="space-y-4">
+                <div>
+                    <InputLabel for="password" value="Kata Sandi" />
+                    <TextInput
+                        id="password"
+                        type="password"
+                        class="mt-1 block w-full"
+                        v-model="form.password"
+                        required
+                        autocomplete="current-password"
+                        autofocus
+                    />
+                    <InputError class="mt-2" :message="form.errors.password" />
+                </div>
+
+                <PrimaryButton
+                    class="w-full justify-center"
+                    :class="{ 'opacity-25': form.processing }"
+                    :disabled="form.processing"
+                >
+                    Konfirmasi
+                </PrimaryButton>
+            </form>
         </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="password" value="Kata Sandi" /> <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                    autofocus
-                />
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="mt-4 flex justify-end">
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Konfirmasi </PrimaryButton>
-            </div>
-        </form>
     </GuestLayout>
 </template>
